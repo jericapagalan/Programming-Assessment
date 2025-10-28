e620bc7de594c3dcf25392ee9665b26094259a89
jericapagalan <jericabathspa0830@gmail.com>
2025-10-28 13:58:03 +0400

    Update Excercise 05.py

--- Excercise 05.py
+++ Excercise 05.py
@@ -21,7 +21,7 @@ try:
     month = int(input("Enter the month number (1-12): "))
 
     if month < 1 or month > 12:
-        print("❌ Invalid month. Please enter a number from 1 to 12.")
+        print(" Invalid month. Please enter a number from 1 to 12.")
     else:
         # If month is February, check for leap year
         if month == 2:
@@ -33,6 +33,7 @@ try:
         else:
             days = month_days[month]
         
-        print(f"✅ The number of days in month {month} is: {days}")
+        print(f" The number of days in month {month} is: {days}")
 except ValueError:
-    print("❌ Invalid input. Please enter a number.")
+
+    print(" Invalid input. Please enter a number.")
